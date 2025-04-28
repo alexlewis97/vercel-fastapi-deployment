23609691ec12b7775d0478dd1800a8b1deecdf2f
alexlewis97 <51166437+alexlewis97@users.noreply.github.com>
2025-04-29 01:04:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ class ChatOutput(BaseModel):
     response: str
     indices_of_chunkrretrived: list[str]
     notes: str
-    performance_tracker: dict[str, Any]
+    performance_tracker: dict[str, any]
 
 if os.getenv("VERCEL_ENV") is None:
     load_dotenv()
